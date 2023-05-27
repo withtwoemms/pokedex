bb364eb6168b0d0df6b1a0044133a33834b16b06
Emmanuel I. Obi <withtwoemms@gmail.com>
2023-05-26 19:31:14 -0500

    adds client function implementations

--- pokedex/api/client.py
+++ pokedex/api/client.py
@@ -1,6 +1,8 @@
+import requests
 from typing import Any, Dict, Generator, Iterable, List, Optional, Union
 
-from pokedex.api.models import PokeApiRequest, PokeApiResourceRef, PokemonRef
+from pokedex.api.constants import BASE_URL
+from pokedex.api.models import PokeApiRequest, PokeApiResource, PokeApiResourceRef, PokemonRef
 
 
 PokeApiEndpoints = Dict[str, str]
@@ -10,20 +12,55 @@ Pokemon = Dict[str, Any]
 
 
 def get_endpoints(endpoints_request: PokeApiRequest) -> PokeApiEndpoints:
-    raise NotImplementedError
+    response: requests.Response = endpoints_request()
+    response.raise_for_status()  # TODO: handle error states
+    endpoints: PokeApiEndpoints = response.json()
+    return endpoints
+
+
+def select_endpoint(name: str, endpoints: PokeApiEndpoints) -> PokeApiRequest:
+    return PokeApiRequest(endpoints[name])
 
 
 def get_pokemon_type_refs(request: PokeApiRequest) -> PokemonTypeRefs:
-    raise NotImplementedError
+    response: requests.Response = request()
+    response.raise_for_status()  # TODO: handle error states
+    pokemon_type_resource = PokeApiResource(**response.json())
+    pokemon_type_refs: List[PokeApiResourceRef] = pokemon_type_resource.results
+    return pokemon_type_refs
+
+
+def select_pokemon_type(
+    pokemon_type: str,
+    pokemon_type_refs: PokemonTypeRefs
+) -> Optional[PokeApiRequest]:
+    for pokemon_type_ref in pokemon_type_refs:
+        if pokemon_type_ref.name == pokemon_type:
+            return pokemon_type_ref.as_request()
 
 
 def get_pokemon_refs(pokemon_type_request: PokeApiRequest) -> Generator[PokemonRef, None, None]:
-    raise NotImplementedError
+    response: requests.Response = pokemon_type_request()
+    response.raise_for_status()  # TODO: handle error states
+    pokemon_refs = response.json()['pokemon']
+    for pokemon_ref in pokemon_refs:
+        yield PokemonRef(**pokemon_ref).as_request()
 
 
+# TODO: make concurrent
 def get_pokemon(pokemon_requests: Iterable[PokeApiRequest]) -> Generator[Pokemon, None, None]:
-    raise NotImplementedError
+    for pokemon_request in pokemon_requests:
+        response: requests.Response = pokemon_request()
+        response.raise_for_status()  # TODO: handle error states
+        yield response.json()
     
 
 def get_pokemon_by_type(pokemon_type: str):
-    raise NotImplementedError
+    pokemon_type_request = select_pokemon_type(
+        pokemon_type,
+        get_pokemon_type_refs(
+            select_endpoint('type', get_endpoints(PokeApiRequest(BASE_URL)))
+        )
+    )
+    pokemon_refs = get_pokemon_refs(pokemon_type_request)
+    yield from get_pokemon(pokemon_refs)
