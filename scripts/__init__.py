9de881485a44f9f606e503f2a47825a438e8bcbe
Emmanuel I. Obi <withtwoemms@gmail.com>
2023-05-29 18:43:52 -0500

    prefixes buildscripts with `python -m`

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -5,6 +5,7 @@ def tests():
     run_tests_and_configure_coverage = [
         # black ignore formatting:
         # fmt: off
+        "python", "-m",
         "coverage", "run", "--source", ".", "--branch", "--omit", "**tests/*,**/__*__.py",
         "-m", "unittest", "discover", "-t", "."
         # fmt: on
@@ -13,7 +14,13 @@ def tests():
 
 
 def coverage():
-    compile_coverage_report = ["coverage", "report", "-m", "--skip-empty", "--fail-under=90"]
+    compile_coverage_report = [
+        # black ignore formatting:
+        # fmt: off
+        "python", "-m",
+        "coverage", "report", "-m", "--skip-empty", "--fail-under=90"
+        # fmt: on
+    ]
     completed_proc = subprocess.run(compile_coverage_report)
     if completed_proc.returncode != 0:
         exit(completed_proc.returncode)
