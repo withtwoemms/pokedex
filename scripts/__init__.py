d29117881ce6a44f2da415aa911dc9befc0a8bef
Emmanuel I. Obi <withtwoemms@gmail.com>
2023-05-29 17:36:29 -0500

    adds coverage console script

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -2,7 +2,21 @@ import subprocess
 
 
 def tests():
-    subprocess.run(["python", "-u", "-m", "unittest", "discover", "-t", "."])
+    run_tests_and_configure_coverage = [
+        # black ignore formatting:
+        # fmt: off
+        "coverage", "run", "--source", ".", "--branch", "--omit", "**tests/*,**/__*__.py",
+        "-m", "unittest", "discover", "-t", "."
+        # fmt: on
+    ]
+    subprocess.run(run_tests_and_configure_coverage)
+
+
+def coverage():
+    compile_coverage_report = ["coverage", "report", "-m", "--skip-empty", "--fail-under=90"]
+    completed_proc = subprocess.run(compile_coverage_report)
+    if completed_proc.returncode != 0:
+        exit(completed_proc.returncode)
 
 
 def lint():
