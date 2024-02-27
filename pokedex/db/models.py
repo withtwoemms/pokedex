9f1bd6e89c4e697063bfe179abf322acdbccb142
Emmanuel I. Obi <withtwoemms@gmail.com>
2024-02-27 14:36:40 -0600

    removes complex type hint

--- pokedex/db/models.py
+++ pokedex/db/models.py
@@ -1,14 +1,11 @@
 import json
 from dataclasses import asdict, dataclass
-from typing import Any, Dict, List, Type, Union
-
-JSON = Union[Dict[str, Any], List[Any], int, str, float, bool, Type[None]]
 
 
 @dataclass
 class Report:
     persisted: dict
 
-    def __str__(self) -> JSON:
+    def __str__(self) -> str:
         summary = dict(asdict(self), count=len(self.persisted))
         return json.dumps(summary, indent=4)
