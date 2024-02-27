0a408dcfc922b9ab1f1a01ab6e552e0004f500d9
Emmanuel I. Obi <withtwoemms@gmail.com>
2024-02-27 14:27:27 -0600

    removes unnecessary default in subcommand

--- pokedex/api/__main__.py
+++ pokedex/api/__main__.py
@@ -1,4 +1,5 @@
 import click
+import json
 
 from pokedex.api.client import get_pokemon_by_move, get_pokemon_by_type
 from pokedex.db.client import persist_requests
@@ -11,8 +12,8 @@ def cli():
 
 
 @cli.command(no_args_is_help=True)
-@click.option("--type", type=click.STRING, default=False, help="e.g. water, grass, fire")
-@click.option("--move", type=click.STRING, default=False, help="e.g. water-gun, razor-leaf, ember")
+@click.option("--type", type=click.STRING, help="e.g. water, grass, fire")
+@click.option("--move", type=click.STRING, help="e.g. water-gun, razor-leaf, ember")
 def by(type: str, move: str):
     """
     Accepts values by which to fetch Pokemon
@@ -20,12 +21,12 @@ def by(type: str, move: str):
     TODO: dismiss mutual exclusivity for given options--
     i.e. `--type` and `--move` should be usable at the same time
     """
-    if type != "False":
+    if type:
         results = dict(persist_requests(get_pokemon_by_type(type)))
         print(Report(persisted=results))
         return
 
-    if move != "False":
+    if move:
         results = dict(persist_requests(get_pokemon_by_move(move)))
         print(Report(persisted=results))
         return
