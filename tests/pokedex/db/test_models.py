d06ee02d5239f0a692690b5ae2ab50894e30890d
Emmanuel I. Obi <withtwoemms@gmail.com>
2024-02-27 11:57:39 -0600

    black formatting

--- tests/pokedex/db/test_models.py
+++ tests/pokedex/db/test_models.py
@@ -5,7 +5,6 @@ from pokedex.db.models import Report
 
 
 class TestReport(TestCase):
-
     def test_report_representation(self):
         results = {
             "https://pokeapi.co/api/v2/pokemon/1/": True,
@@ -13,9 +12,6 @@ class TestReport(TestCase):
             "https://pokeapi.co/api/v2/pokemon/3/": False,
         }
         report = Report(persisted=results)
-        expected_report = json.dumps(
-            {"persisted": results, "count": len(results)},
-            indent=4
-        )
+        expected_report = json.dumps({"persisted": results, "count": len(results)}, indent=4)
 
         assert str(report) == expected_report
